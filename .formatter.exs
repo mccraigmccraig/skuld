5ac526484c303280c13ca19b1dea2607287a5ac1
mccraigmccraig <mccraigmccraig@gmail.com>
2026-03-27 20:58:17 +0000

    defport doesn't need parens

--- .formatter.exs
+++ .formatter.exs
@@ -3,7 +3,14 @@
   import_deps: [],
   plugins: [],
   heex_line_length: 80,
-  locals_without_parens: [attr: 2, attr: 3, def_op: 1, def_op_struct: :*, def_tagged_op: 1],
+  locals_without_parens: [
+    attr: 2,
+    attr: 3,
+    def_op: 1,
+    def_op_struct: :*,
+    def_tagged_op: 1,
+    defport: :*
+  ],
   inputs: [
     "{mix,.formatter}.exs",
     "*.{heex,ex,exs}",
