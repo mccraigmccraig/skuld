756cb022a26024a889da5ccff76e207f24b84a2e
mccraigmccraig <mccraigmccraig@gmail.com>
2026-03-31 17:36:42 +0100

    update hex_port -> v0.11.0

--- mix.exs
+++ mix.exs
@@ -180,7 +180,7 @@ defmodule Skuld.MixProject do
 
   defp deps do
     [
-      {:hex_port, "~> 0.10"},
+      {:hex_port, "~> 0.11"},
       {:jason, "~> 1.4"},
       {:uniq, "~> 0.6"},
       {:ecto, "~> 3.12", optional: true},
