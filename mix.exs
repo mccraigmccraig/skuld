a4825a452b3de2b582550a20a0f58cdecbf2d114
mccraigmccraig <mccraigmccraig@gmail.com>
2026-05-03 12:31:46 +0100

    double_down -> 0.58.0

--- mix.exs
+++ mix.exs
@@ -180,7 +180,7 @@ defmodule Skuld.MixProject do
 
   defp deps do
     [
-      {:double_down, "~> 0.54"},
+      {:double_down, "~> 0.58"},
       {:jason, "~> 1.4"},
       {:uniq, "~> 0.6"},
       {:ecto, "~> 3.12", optional: true},
